dc9a021884b05893b6e9f05dd9d89e11320e4d90
Yi Xing <blandilyte@gmail.com>
2024-07-01 14:29:24 +0800

    Fix translator usage

--- src/main.cc
+++ src/main.cc
@@ -10,18 +10,16 @@ int main(int argc, char * argv[])
 	QApplication a(argc, argv);
 
 	QTranslator translator;
-	const QStringList uiLanguages = QLocale::system().uiLanguages();
-	for (const QString & locale : uiLanguages)
+	for (const QString & locale : QLocale::system().uiLanguages())
 	{
-		const QString baseName = "SilverDict_" + QLocale(locale).name();
-		qDebug() << "Trying to load translation for" << baseName;
-		if (translator.load("/data/translations/" + baseName))
+		if (translator.load(QLocale(locale), "SilverDict", "_", "./translations"))
 		{
-			a.installTranslator(&translator);
 			qDebug() << "Loaded translation for" << locale;
+			a.installTranslator(&translator);
 			break;
 		}
 	}
+
 	MainWindow w;
 	w.show();
 	return a.exec();
