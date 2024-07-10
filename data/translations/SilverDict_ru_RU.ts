f159c50c17503b9147e2018f1e2f5bd7d8e16441
Yi Xing <91039086+Crissium@users.noreply.github.com>
2024-07-10 16:39:43 +0800

    New translations silverdict_en_gb.ts (Russian) New translations {languageName}

--- data/translations/SilverDict_ru_RU.ts
+++ data/translations/SilverDict_ru_RU.ts
@@ -6,7 +6,7 @@
     <message>
       <location filename="../../src/about/aboutdialogue.ui" line="20"/>
       <source>About</source>
-      <translation type="unfinished">About</translation>
+      <translation>О программе</translation>
     </message>
     <message>
       <location filename="../../src/about/aboutdialogue.ui" line="45"/>
@@ -29,27 +29,27 @@
     <message>
       <location filename="../../src/edit/adddictionarydialog.ui" line="14"/>
       <source>New Dictionary</source>
-      <translation type="unfinished">New Dictionary</translation>
+      <translation>Новый словарь</translation>
     </message>
     <message>
       <location filename="../../src/edit/adddictionarydialog.ui" line="26"/>
       <source>Name</source>
-      <translation type="unfinished">Name</translation>
+      <translation>Имя</translation>
     </message>
     <message>
       <location filename="../../src/edit/adddictionarydialog.ui" line="39"/>
       <source>Filename</source>
-      <translation type="unfinished">Filename</translation>
+      <translation>Имя файла</translation>
     </message>
     <message>
       <location filename="../../src/edit/adddictionarydialog.ui" line="61"/>
       <source>Format</source>
-      <translation type="unfinished">Format</translation>
+      <translation>Формат</translation>
     </message>
     <message>
       <location filename="../../src/edit/adddictionarydialog.ui" line="74"/>
       <source>Group</source>
-      <translation type="unfinished">Group</translation>
+      <translation>Группа</translation>
     </message>
   </context>
   <context>
@@ -57,22 +57,22 @@
     <message>
       <location filename="../../src/edit/addgroupdialog.ui" line="14"/>
       <source>New Group</source>
-      <translation type="unfinished">New Group</translation>
+      <translation>Новая группа</translation>
     </message>
     <message>
       <location filename="../../src/edit/addgroupdialog.ui" line="35"/>
       <source>Group Name</source>
-      <translation type="unfinished">Group Name</translation>
+      <translation>Имя группы</translation>
     </message>
     <message>
       <location filename="../../src/edit/addgroupdialog.ui" line="54"/>
       <source>Group Language(s)</source>
-      <translation type="unfinished">Group Language(s)</translation>
+      <translation>Язык(и) группы</translation>
     </message>
     <message>
       <location filename="../../src/edit/addgroupdialog.ui" line="67"/>
       <source>A comma separated list of two-letter ISO-639-1 language codes.</source>
-      <translation type="unfinished">A comma separated list of two-letter ISO-639-1 language codes.</translation>
+      <translation>Список двухбуквенных кодов языков ISO-639-1, разделенных запятыми.</translation>
     </message>
   </context>
   <context>
@@ -80,7 +80,7 @@
     <message>
       <location filename="../../src/query/articleview.ui" line="177"/>
       <source>about:blank</source>
-      <translation type="unfinished">about:blank</translation>
+      <translation>about:blank</translation>
     </message>
     <message>
       <location filename="../../src/query/articleview.cc" line="55"/>
@@ -92,7 +92,7 @@
       <location filename="../../src/query/articleview.cc" line="69"/>
       <location filename="../../src/query/articleview.cc" line="312"/>
       <source>Error</source>
-      <translation type="unfinished">Error</translation>
+      <translation>Ошибка</translation>
     </message>
     <message>
       <location filename="../../src/query/articleview.cc" line="70"/>
@@ -102,12 +102,12 @@
     <message>
       <location filename="../../src/query/articleview.cc" line="220"/>
       <source>Open link in new tab</source>
-      <translation type="unfinished">Open link in new tab</translation>
+      <translation>Открыть ссылку в новой вкладке</translation>
     </message>
     <message>
       <location filename="../../src/query/articleview.cc" line="228"/>
       <source>Open link in browser</source>
-      <translation type="unfinished">Open link in browser</translation>
+      <translation>Открыть ссылку в браузере</translation>
     </message>
     <message>
       <location filename="../../src/query/articleview.cc" line="249"/>
@@ -117,12 +117,12 @@
     <message>
       <location filename="../../src/query/articleview.cc" line="255"/>
       <source>Search</source>
-      <translation type="unfinished">Search</translation>
+      <translation>Поиск</translation>
     </message>
     <message>
       <location filename="../../src/query/articleview.cc" line="261"/>
       <source>Search in new tab</source>
-      <translation type="unfinished">Search in new tab</translation>
+      <translation>Поиск в новой вкладке</translation>
     </message>
     <message>
       <location filename="../../src/query/articleview.cc" line="313"/>
@@ -135,58 +135,58 @@
     <message>
       <location filename="../../src/edit/dictionarydialog.ui" line="20"/>
       <source>Dictionaries</source>
-      <translation type="unfinished">Dictionaries</translation>
+      <translation>Словари</translation>
     </message>
     <message>
       <location filename="../../src/edit/dictionarydialog.ui" line="81"/>
       <source>Name</source>
-      <translation type="unfinished">Name</translation>
+      <translation>Имя</translation>
     </message>
     <message>
       <location filename="../../src/edit/dictionarydialog.ui" line="99"/>
       <source>Filename</source>
-      <translation type="unfinished">Filename</translation>
+      <translation>Имя файла</translation>
     </message>
     <message>
       <location filename="../../src/edit/dictionarydialog.ui" line="117"/>
       <source>Format</source>
-      <translation type="unfinished">Format</translation>
+      <translation>Формат</translation>
     </message>
     <message>
       <location filename="../../src/edit/dictionarydialog.ui" line="135"/>
       <source>Headword Count</source>
-      <translation type="unfinished">Headword Count</translation>
+      <translation>Количество слов</translation>
     </message>
     <message>
       <location filename="../../src/edit/dictionarydialog.ui" line="162"/>
       <source>Rename</source>
-      <translation type="unfinished">Rename</translation>
+      <translation>Переименовать</translation>
     </message>
     <message>
       <location filename="../../src/edit/dictionarydialog.ui" line="184"/>
       <source>Add Dictionary</source>
-      <translation type="unfinished">Add Dictionary</translation>
+      <translation>Добавить словарь</translation>
     </message>
     <message>
       <location filename="../../src/edit/dictionarydialog.ui" line="206"/>
       <location filename="../../src/edit/dictionarydialog.cc" line="115"/>
       <source>Delete</source>
-      <translation type="unfinished">Delete</translation>
+      <translation>Удалить</translation>
     </message>
     <message>
       <location filename="../../src/edit/dictionarydialog.cc" line="72"/>
       <source>Loading...</source>
-      <translation type="unfinished">Loading...</translation>
+      <translation>Загрузка...</translation>
     </message>
     <message>
       <location filename="../../src/edit/dictionarydialog.cc" line="92"/>
       <source>Rename dictionary</source>
-      <translation type="unfinished">Rename dictionary</translation>
+      <translation>Переименовать словарь</translation>
     </message>
     <message>
       <location filename="../../src/edit/dictionarydialog.cc" line="116"/>
       <source>Are you sure you want to delete ‘%1’? The dictionary files won&apos;t be deleted.</source>
-      <translation type="unfinished">Are you sure you want to delete ‘%1’? The dictionary files won&apos;t be deleted.</translation>
+      <translation>Вы уверены, что хотите удалить '%1'? Файлы словаря не будут удалены.</translation>
     </message>
   </context>
   <context>
@@ -194,42 +194,42 @@
     <message>
       <location filename="../../src/edit/groupdialog.ui" line="20"/>
       <source>Groups</source>
-      <translation type="unfinished">Groups</translation>
+      <translation>Группы</translation>
     </message>
     <message>
       <location filename="../../src/edit/groupdialog.ui" line="53"/>
       <source>Group name</source>
-      <translation type="unfinished">Group name</translation>
+      <translation>Имя группы</translation>
     </message>
     <message>
       <location filename="../../src/edit/groupdialog.ui" line="62"/>
       <source>Group language(s)</source>
-      <translation type="unfinished">Group language(s)</translation>
+      <translation>Язык(и) группы</translation>
     </message>
     <message>
       <location filename="../../src/edit/groupdialog.ui" line="71"/>
       <source>Dictionaries</source>
-      <translation type="unfinished">Dictionaries</translation>
+      <translation>Словари</translation>
     </message>
     <message>
       <location filename="../../src/edit/groupdialog.ui" line="95"/>
       <source>Rename</source>
-      <translation type="unfinished">Rename</translation>
+      <translation>Переименовать</translation>
     </message>
     <message>
       <location filename="../../src/edit/groupdialog.ui" line="114"/>
       <source>Edit Languages</source>
-      <translation type="unfinished">Edit Languages</translation>
+      <translation>Редактировать языки</translation>
     </message>
     <message>
       <location filename="../../src/edit/groupdialog.ui" line="133"/>
       <source>Edit Dictionaries</source>
-      <translation type="unfinished">Edit Dictionaries</translation>
+      <translation>Редактировать словари</translation>
     </message>
     <message>
       <location filename="../../src/edit/groupdialog.ui" line="152"/>
       <source>Add Group</source>
-      <translation type="unfinished">Add Group</translation>
+      <translation>Добавить группу</translation>
     </message>
     <message>
       <location filename="../../src/edit/groupdialog.ui" line="171"/>
@@ -242,27 +242,27 @@
     <message>
       <location filename="../../src/mainwindow.ui" line="14"/>
       <source>SilverDict</source>
-      <translation type="unfinished">SilverDict</translation>
+      <translation>SilverDict</translation>
     </message>
     <message>
       <location filename="../../src/mainwindow.ui" line="36"/>
       <source>File</source>
-      <translation type="unfinished">File</translation>
+      <translation>Файл</translation>
     </message>
     <message>
       <location filename="../../src/mainwindow.ui" line="43"/>
       <source>Edit</source>
-      <translation type="unfinished">Edit</translation>
+      <translation>Правка</translation>
     </message>
     <message>
       <location filename="../../src/mainwindow.ui" line="52"/>
       <source>Help</source>
-      <translation type="unfinished">Help</translation>
+      <translation>Справка</translation>
     </message>
     <message>
       <location filename="../../src/mainwindow.ui" line="66"/>
       <source>Quit</source>
-      <translation type="unfinished">Quit</translation>
+      <translation>Выход</translation>
     </message>
     <message>
       <location filename="../../src/mainwindow.ui" line="77"/>
@@ -272,32 +272,32 @@
     <message>
       <location filename="../../src/mainwindow.ui" line="88"/>
       <source> About</source>
-      <translation type="unfinished"> About</translation>
+      <translation> О программе</translation>
     </message>
     <message>
       <location filename="../../src/mainwindow.ui" line="99"/>
       <source>Dictionaries</source>
-      <translation type="unfinished">Dictionaries</translation>
+      <translation>Словари</translation>
     </message>
     <message>
       <location filename="../../src/mainwindow.ui" line="110"/>
       <source>Groups</source>
-      <translation type="unfinished">Groups</translation>
+      <translation>Группы</translation>
     </message>
     <message>
       <location filename="../../src/mainwindow.ui" line="121"/>
       <source>Preferences</source>
-      <translation type="unfinished">Preferences</translation>
+      <translation>Параметры</translation>
     </message>
     <message>
       <location filename="../../src/mainwindow.ui" line="132"/>
       <source>Sources</source>
-      <translation type="unfinished">Sources</translation>
+      <translation>Источники</translation>
     </message>
     <message>
       <location filename="../../src/mainwindow.ui" line="140"/>
       <source>Close to tray</source>
-      <translation type="unfinished">Close to tray</translation>
+      <translation>Свернуть в лоток</translation>
     </message>
     <message>
       <location filename="../../src/mainwindow.cc" line="29"/>
@@ -307,17 +307,17 @@
     <message>
       <location filename="../../src/mainwindow.cc" line="60"/>
       <source>Please enter the server address</source>
-      <translation type="unfinished">Please enter the server address</translation>
+      <translation>Пожалуйста, введите адрес сервера</translation>
     </message>
     <message>
       <location filename="../../src/mainwindow.cc" line="74"/>
       <source>Error</source>
-      <translation type="unfinished">Error</translation>
+      <translation>Ошибка</translation>
     </message>
     <message>
       <location filename="../../src/mainwindow.cc" line="74"/>
       <source>Failed to initialise application.</source>
-      <translation type="unfinished">Failed to initialise application.</translation>
+      <translation>Не удалось инициализировать приложение.</translation>
     </message>
   </context>
   <context>
@@ -325,7 +325,7 @@
     <message>
       <location filename="../../src/preferences/preferencesdialogue.ui" line="20"/>
       <source>Preferences</source>
-      <translation type="unfinished">Preferences</translation>
+      <translation>Параметры</translation>
     </message>
     <message>
       <location filename="../../src/preferences/preferencesdialogue.ui" line="30"/>
@@ -355,22 +355,22 @@
     <message>
       <location filename="../../src/preferences/preferencesdialogue.ui" line="96"/>
       <source>History Size</source>
-      <translation type="unfinished">History Size</translation>
+      <translation>Размер истории</translation>
     </message>
     <message>
       <location filename="../../src/preferences/preferencesdialogue.ui" line="116"/>
       <source>Suggestion Size</source>
-      <translation type="unfinished">Suggestion Size</translation>
+      <translation>Размер предложения</translation>
     </message>
     <message>
       <location filename="../../src/preferences/preferencesdialogue.ui" line="136"/>
       <source>Clear history</source>
-      <translation type="unfinished">Clear history</translation>
+      <translation>Очистить историю</translation>
     </message>
     <message>
       <location filename="../../src/preferences/preferencesdialogue.ui" line="143"/>
       <source>Create n-gram index</source>
-      <translation type="unfinished">Create n-gram index</translation>
+      <translation>Создать индекс n-gram</translation>
     </message>
     <message>
       <location filename="../../src/preferences/preferencesdialogue.cc" line="10"/>
@@ -389,7 +389,7 @@
       <location filename="../../src/preferences/preferencesdialogue.cc" line="92"/>
       <location filename="../../src/preferences/preferencesdialogue.cc" line="112"/>
       <source>Error</source>
-      <translation type="unfinished">Error</translation>
+      <translation>Ошибка</translation>
     </message>
     <message>
       <location filename="../../src/preferences/preferencesdialogue.cc" line="39"/>
@@ -409,12 +409,12 @@
     <message>
       <location filename="../../src/preferences/preferencesdialogue.cc" line="78"/>
       <source>Export History</source>
-      <translation type="unfinished">Export History</translation>
+      <translation>История экспорта</translation>
     </message>
     <message>
       <location filename="../../src/preferences/preferencesdialogue.cc" line="80"/>
       <source>Text Files (*.txt)</source>
-      <translation type="unfinished">Text Files (*.txt)</translation>
+      <translation>Текстовые файлы (*.txt)</translation>
     </message>
     <message>
       <location filename="../../src/preferences/preferencesdialogue.cc" line="93"/>
@@ -424,7 +424,7 @@
     <message>
       <location filename="../../src/preferences/preferencesdialogue.cc" line="113"/>
       <source>Failed to create n-gram index.</source>
-      <translation type="unfinished">Failed to create n-gram index.</translation>
+      <translation>Не удалось создать индекс n-gram.</translation>
     </message>
   </context>
 </TS>
